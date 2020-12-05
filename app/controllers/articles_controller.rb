58b7a8eefb2005cf6c965eeaa5b822989122aaa6
Charlie Bertrand <charlie.bertrand@live.com>
2020-12-05 17:55:17 +0100

    editing done, missing destory

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,35 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params_article)
+    @article.save
+    redirect_to articles_path(@article)
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(params_article)
+    redirect_to article_path(@article)
+  end
+
+  private
+
+  def params_article
+    params.require(:article).permit(:title, :content)
   end
 end
